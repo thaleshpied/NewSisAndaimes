979413957cc055bac435206c2f86731be1c83d43
Thales Henrique <thaleshpied@gmail.com>
2023-11-11 17:01:15 -0300

    Configurações

--- SisAndaimes-main/application/config/config.php
+++ SisAndaimes-main/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/newsisandaime';
+$config['base_url'] = 'http://localhost/NewSisAndaimes/SisAndaimes-main/';
 
 
 /*
@@ -79,7 +79,7 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']	= 'portuguese-br';
+$config['language']	= '';
 
 /*
 |--------------------------------------------------------------------------
